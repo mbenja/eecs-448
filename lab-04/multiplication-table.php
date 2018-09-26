23bfb9011021ed84152324a0bb17a535139c1973
Benjamin Streit <benjaminmstreit@gmail.com>
2018-09-25 23:07:57 -0500

    Removing multiplication-table.html for now. Implemented multiplication table exercise in multiplication-table.php.

--- lab-04/multiplication-table.php
+++ lab-04/multiplication-table.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: benja
- * Date: 9/25/18
- * Time: 10:00 AM
- */
+
+echo "<table>";
+
+for ($i = 0; $i <= 100; $i++) {
+  echo "<tr>";
+  for ($j = 0; $j <= 100; $j++) {
+    if ($i == 0 && $j == 0) {
+      echo "<td/>";
+    } elseif ($i == 0) {
+      echo "<td>" . $j . "<td/>";
+    } elseif ($j == 0) {
+      echo "<td>" . $i . "<td/>";
+    } else {
+      echo "<td>" . ($i * $j) . "<td/>";
+    }
+  }
+  echo "<tr/>";
+}
+
+echo "<table/>";
+
+?>
