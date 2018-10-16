4d741a0027daafffcf6bb116962c2e791f63f906
Benjamin Streit <benjaminmstreit@gmail.com>
2018-10-15 21:45:44 -0500

    Create user interface and back-end implemented.

--- lab-05/js/createUser.js
+++ lab-05/js/createUser.js
@@ -7,17 +7,3 @@ const modal_manager = new ModalManager();
   * Call for css manipulation modal to be presented
 */
 modal_manager.operationCreateUser();
-
-/**
-  * Handles submission of create user form
-  * Presents success and error modals accordingly
-*/
-function createUser() {
-  $.ajax({
-      type: "POST",
-      url: "../php/createUser.php",
-      success: function(data) {
-          console.log(data);
-      }
-  });
-}
--- lab-05/php/createUser.php
+++ lab-05/php/createUser.php
@@ -1,7 +1,40 @@
 <?php
-//access the global array called $_POST to get the values from the text fields
-$user = $_POST["input_user"];
 
-echo "User: " . user . "<br>";
+// connect to db
+$mysql_connection = new mysqli("mysql.eecs.ku.edu", "b265s866", "dov7Naph", "b265s866");
+/* check connection */
+if ($mysql_connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysql_connection->connect_error);
+    exit();
+}
+
+// get input
+$user = $_POST["input_user"];
+// check if input is empty
+if ($user == "") {
+  echo "Whoops, input some text.<br>";
+  return;
+}
+
+// create query string, perform query
+$query = "SELECT * FROM `Users` WHERE `user_id`='" . $user . "';";
+$users = $mysql_connection->query($query);
+
+if ($users->num_rows > 0) {
+  // user exists already
+  echo "User already exists.";
+} else {
+  // user does not exist, insert
+  $query = "INSERT INTO `Users` (user_id) VALUES ('" . $user . "');";
+
+  if ($mysql_connection->query($query) === TRUE) {
+      echo "New user created.";
+  } else {
+      echo "Error: " . $query . "<br>" . $mysql_connection->error;
+  }
+}
+
+// close connection
+$mysql_connection->close();
 
 ?>
